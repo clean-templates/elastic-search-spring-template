686c4ee9b223f0b7766ed1b5cb1fe6f067ec9e63
RolandSall <roland.salloum00@outlook.com>
2023-05-01 10:29:13 +0300

    fix: query service injection

--- src/main/java/com/rolandsall/elastic/search/spring/template/core/application/comment/command/CommentCommandService.java
+++ src/main/java/com/rolandsall/elastic/search/spring/template/core/application/comment/command/CommentCommandService.java
@@ -2,6 +2,7 @@ package com.rolandsall.elastic.search.spring.template.core.application.comment.c
 
 import com.rolandsall.elastic.search.spring.template.core.application.comment.command.dto.CommentRequest;
 import com.rolandsall.elastic.search.spring.template.core.application.post.query.IPostQueryService;
+import com.rolandsall.elastic.search.spring.template.core.domain.Post;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -9,10 +10,10 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class CommentCommandService implements ICommentCommandService {
 
-    private IPostQueryService postQueryService;
+    private final IPostQueryService postQueryService;
 
     @Override
     public void addComment(String postId, CommentRequest request) {
-        postQueryService.findById(postId);
+        Post post = postQueryService.findById(postId);
     }
 }
